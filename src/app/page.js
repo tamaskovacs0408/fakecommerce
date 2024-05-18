74bc59fc090f0cf1d6ccaec44287af85b2246193
Tamás Kovács <kovacs.tamas0408@gmail.com>
2024-05-18 12:31:04 +0200

    get products

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,13 @@
-export default function Home() {
+import Product from './components/Product';
+
+import { getProducts } from './lib/datamanagement';
+
+export default async function Home() {
+  const products = await getProducts();
+
   return(
     <main>
-      Home page
+      {products.map(product => <Product key={product.id} product={product}/>)}
     </main>
   )
 }
