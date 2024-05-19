a13410e42084cdb4f284337b9a79f4d76bedba96
Tamás Kovács <kovacs.tamas0408@gmail.com>
2024-05-19 20:35:11 +0200

    Relocate

--- src/app/lib/datamanagement.js
+++
@@ -1,6 +0,0 @@
-export async function getProducts() {
-    const data = await fetch('https://fakestoreapi.com/products');
-    const response = data.json();
-  
-    return response;
-  }
---
+++ src/lib/datamanagement.js
@@ -0,0 +1,6 @@
+export async function getProducts() {
+  const data = await fetch("https://fakestoreapi.com/products");
+  const response = data.json();
+
+  return response;
+}
