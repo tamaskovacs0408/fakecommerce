95bcb36bedcf88a0657d03f95d97b3ef9366d6c3
Tamás Kovács <kovacs.tamas0408@gmail.com>
2024-05-19 20:35:22 +0200

    Add tailwind styles

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
-import Product from './components/Product';
+import Product from '../components/Product';
 
-import { getProducts } from './lib/datamanagement';
+import { getProducts } from '../lib/datamanagement';
 
 export default async function Home() {
   const products = await getProducts();
 
   return(
-    <main>
+    <main className='grid grid-cols-4 justify-items-center items-center'>
       {products.map(product => <Product key={product.id} product={product}/>)}
     </main>
   )
